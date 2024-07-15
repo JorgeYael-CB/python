3caabea3201a6ae2eb86b4ba8cf97b5618246a23
JorgeYael-CB <jorge.yael333@gmail.com>
2024-07-14 21:57:53 -0600

    Agregamos la ordenacion de palabra

--- 05-day/problemas-practicos.py
+++ 05-day/problemas-practicos.py
@@ -8,9 +8,9 @@ def devolver_distintos(par1: int, par2: int, par3: int):
 
   for num in array:
     if num > num_max:
-        num_max = num
+      num_max = num
     if num < num_min:
-        num_min = num
+      num_min = num
     suma += num
 
   if suma > 15:
@@ -21,7 +21,19 @@ def devolver_distintos(par1: int, par2: int, par3: int):
     return suma // len(array)
 
 
+def ordenar_palabra( palabra: str ):
+  lista = list( palabra )
+  lista.sort()
 
-print( devolver_distintos(10, 20, 30) ) # 30
-print( devolver_distintos(1, 2, 3) ) # 1
-print( devolver_distintos(5, 5, 1) ) # 3
+  palabras = set( lista )
+  print( palabras )
+
+
+
+
+# ordenar_palabra('Hello World')
+
+# print( devolver_distintos(10, 20, 30) ) # 30
+# print( devolver_distintos(1, 2, 3) ) # 1
+# print( devolver_distintos(5, 5, 1) ) # 3
+
